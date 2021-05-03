368ea5c4ba4b71c80b0a4c636721114e7bea8984
kimjungi <yajungi@hanmail.net>
2021-05-03 23:00:29 +0900

    Add Product entity

---
+++ src/main/java/com/coha/toy/reservation/product/entity/Product.java
@@ -0,0 +1,35 @@
+package com.coha.toy.reservation.product.entity;
+
+import com.coha.toy.reservation.category.entity.Category;
+import com.coha.toy.reservation.common.entity.BaseTime;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="product")
+@Getter
+public class Product extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "event")
+    private String event;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
