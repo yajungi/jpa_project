925dbd6435509a69ace729a28f6f75d1538b7d1d
kimjungi <yajungi@hanmail.net>
2021-06-26 00:48:19 +0900

    Modified CategoryController
    ResponseEntity 로 return type 변경

--- src/main/java/com/coha/toy/reservation/category/controller/CategoryController.java
+++ src/main/java/com/coha/toy/reservation/category/controller/CategoryController.java
@@ -1,10 +1,17 @@
 package com.coha.toy.reservation.category.controller;
 
+import com.coha.toy.reservation.category.dto.CategoryDto;
+import com.coha.toy.reservation.category.dto.CategoryResult;
 import com.coha.toy.reservation.category.service.CategoryService;
 import com.coha.toy.reservation.common.entity.ApiResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.coha.toy.reservation.common.entity.ApiResult.succeed;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,8 +20,10 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/api/categories")
-    public ApiResult getCategories() {
-        return ApiResult.succeed(categoryService.getCategories());
+    public ResponseEntity<ApiResult> getCategories() {
+        List<CategoryDto> categoryDtos = categoryService.getCategories();
+
+        return ResponseEntity.ok(succeed(new CategoryResult(categoryDtos.size(), categoryDtos)));
     }
 
 }
